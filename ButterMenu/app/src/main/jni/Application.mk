0b0984795cb24a4822af8dbdc128df61a937a33a
butterbobo <butterbobo7766@gmail.com>
2023-11-02 14:01:09 +0800

    Delete ButterMenu directory

--- ButterMenu/app/src/main/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-#APP_PLATFORM does not need to be set. It will automatically defaulting
-#APP_PLATFORM := SdkVersion 29 
-
-APP_ABI := armeabi-v7a
-APP_STL := c++_static
-APP_OPTIM := release
-APP_THIN_ARCHIVE := true
-APP_PIE := true
